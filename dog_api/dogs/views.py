f89fbf2bbecadc84ecf086980ced462c8c1d919f
Shakil-RU <shakilnirob111@gmail.com>
2023-04-09 00:13:26 +0600

    dog-api added

---
+++ dog_api/dogs/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+# from backend.dog_api.dogs.serializers import BreedSerializer
+
+# Create your views here.
+from .models import Dog
+from .models import Breed
+from dogs.serializers import DogSerializer
+from dogs.serializers import BreedSerializer
+
+class ListDogs(generics.ListCreateAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+    name = 'dog-list'
+
+
+class DetailDog(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+    name = 'dog-detail'
+
+
+class ListBreeds(generics.ListCreateAPIView):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+    name = 'breed-list'
+
+
+class DetailBreed(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+    name = 'breed-detail'
